ccc1e13b10a4a0e30b2b44f8d698a765a65033b6
Julian Vogel <birdman.coding.1991@gmail.com>
2023-04-06 11:49:39 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useTranslation, Trans } from "react-i18next";
+
+type Lngs = {
+  [key: string]: {
+    nativeName: string;
+  };
+};
+
+const lngs: Lngs = {
+  en: { nativeName: "English" },
+  de: { nativeName: "Deutsch" },
+};
+
+function App() {
+  const { t, i18n } = useTranslation(["pastry"]);
+  return (
+    <div className="App">
+      <div>
+        {Object.keys(lngs).map((key) => (
+          <button
+            key={key}
+            onClick={() => i18n.changeLanguage(key)}
+            disabled={i18n.resolvedLanguage === key}
+          >
+            {lngs[key].nativeName}
+          </button>
+        ))}
+      </div>
+      <h1>{t("cake")}</h1>
+      <p>
+        <Trans i18nKey="pastry:description">Default value</Trans>
+      </p>
+    </div>
+  );
+}
+
+export default App;
